c057ef35c279551411e7481fe748b10f04c2ec92
Dhiraj Tiwari <dhirajtiwari@Dhirajs-Mac-mini.local>
2023-11-21 02:44:08 -0800

    first ai recommit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+import streamlit as st
+
+def main():
+   print("hello AI")
+   st.set_page_config(
+       page_title="my first AI chat",
+       page_icon="now"
+   )
+
+   if __name__ == '__main__':
+       main()
