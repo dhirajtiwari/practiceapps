059325af37f9191ffdf31adea4f478a2a0784fee
dhirajtiwari <dhirajkumartiwari@gmail.com>
2023-11-21 03:34:17 -0800

    firstai git two

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-
-
-
-import streamlit as st
-
 def main():
    print("hello AI")
-   st.set_page_config(
-       page_title="my first AI chat",
-       page_icon="now"
-   )
-
-   if __name__ == '__main__':
+ 
+   
+   if __name__ == "__main__":
        main()
+
+       print("Hello, World!")
